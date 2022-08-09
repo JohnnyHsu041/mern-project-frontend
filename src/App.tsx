5a53d632bea50f737097a98539eb2058532d8c9a
Johnny Hsu <a209328641@gmail.com>
2022-08-09 17:27:12 +0800

    folders setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import NewPlace from "./places/pages/NewPlace";
+import Users from "./user/pages/Users";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact></Route>
+                <Route path="/user" exact>
+                    <Users />
+                </Route>
+                <Route path="/places/new" exact>
+                    <NewPlace />
+                </Route>
+                <Redirect to="/" />
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/places/pages/NewPlace.tsx
@@ -0,0 +1,5 @@
+const NewPlace: React.FC = () => {
+    return <h2>New place works!</h2>;
+};
+
+export default NewPlace;
