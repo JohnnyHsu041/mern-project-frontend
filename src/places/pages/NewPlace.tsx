748561df867324b402588ce9bd4a1cbb992ba4f6
Johnny Hsu <a209328641@gmail.com>
2022-08-24 14:32:59 +0800

    created validators util

--- src/places/pages/NewPlace.tsx
+++ src/places/pages/NewPlace.tsx
@@ -1,10 +1,17 @@
+import { VALIDATOR_REQUIRE } from "../../shared/utils/validators";
 import Input from "../../shared/components/FormElements/Input";
 import classes from "./NewPlace.module.css";
 
 const NewPlace: React.FC = () => {
     return (
         <form className={classes["place-form"]}>
-            <Input element="input" type="text" label="Title" />
+            <Input
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid title"
+            />
         </form>
     );
 };
--- src/shared/components/FormElements/Input.tsx
+++ src/shared/components/FormElements/Input.tsx
@@ -1,3 +1,6 @@
+import React, { useReducer } from "react";
+
+import { validate } from "../../utils/validators";
 import classes from "./Input.module.css";
 
 interface InputProps {
@@ -7,7 +10,42 @@ interface InputProps {
     placeholder?: string;
     rows?: number;
     label?: string;
+    errorText?: string;
+    validators: { type: string; val?: number }[];
 }
+
+type inputState = {
+    value: string;
+    isValid: boolean;
+    isTouched: boolean;
+};
+
+const inputReducer = (
+    state: inputState,
+    action: {
+        type: string;
+        val: string;
+        validators: { type: string; val?: number }[];
+    }
+) => {
+    switch (action.type) {
+        case "CHANGE":
+            return {
+                ...state,
+                value: action.val,
+                isValid: validate(action.val!, action.validators!),
+            };
+
+        case "TOUCHED":
+            return {
+                ...state,
+                isTouched: true,
+            };
+
+        default:
+            return state;
+    }
+};
 
 const Input: React.FC<InputProps> = ({
     element,
@@ -16,18 +54,64 @@ const Input: React.FC<InputProps> = ({
     placeholder,
     rows,
     label,
+    errorText,
+    validators,
 }) => {
+    const [enteredInput, dispatch] = useReducer(inputReducer, {
+        value: "",
+        isValid: false,
+        isTouched: false,
+    });
+
+    const changeHandler = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        dispatch({
+            type: "CHANGE",
+            val: event.target.value,
+            validators: validators,
+        });
+    };
+
+    const touchHandler = () => {
+        dispatch({ type: "TOUCHED", val: "", validators: [{ type: "" }] });
+    };
+
     const renderElement =
         element === "input" ? (
-            <input id={id} type={type} placeholder={placeholder} />
+            <input
+                id={id}
+                type={type}
+                placeholder={placeholder}
+                onChange={changeHandler}
+                onBlur={touchHandler}
+                value={enteredInput.value}
+            />
         ) : (
-            <textarea id={id} rows={rows || 3} />
+            <textarea
+                id={id}
+                rows={rows || 3}
+                value={enteredInput.value}
+                onBlur={touchHandler}
+            />
         );
 
     return (
-        <div className={`${classes["form-control"]}`}>
+        <div
+            className={`
+                ${classes["form-control"]} 
+                ${
+                    !enteredInput.isValid &&
+                    enteredInput.isTouched &&
+                    classes["form-control--invalid"]
+                }
+            `}
+        >
             <label htmlFor={id}>{label}</label>
             {renderElement}
+            {!enteredInput.isValid && enteredInput.isTouched && (
+                <p>{errorText}</p>
+            )}
         </div>
     );
 };
