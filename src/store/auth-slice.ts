ce155a5777f1f6757664258fc1b12296eace2676
Johnny Hsu <a209328641@gmail.com>
2022-09-06 21:23:07 +0800

    added userId into authSlice for redux

--- src/store/auth-slice.ts
+++ src/store/auth-slice.ts
@@ -2,21 +2,25 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export interface Auth {
     isLoggedIn: boolean;
+    userId: string | null;
 }
 
 const AuthInitialState: Auth = {
     isLoggedIn: false,
+    userId: null,
 };
 
 export const authSlice = createSlice({
     name: "auth",
     initialState: AuthInitialState,
     reducers: {
-        login(state) {
+        login(state, action) {
             state.isLoggedIn = true;
+            state.userId = action.payload;
         },
         logout(state) {
             state.isLoggedIn = false;
+            state.userId = null;
         },
     },
 });
